83880d68aa2f01ea392f5f14f47e973244e74c1b
Mor Ndiaye <morn5574@gmail.com>
2025-07-16 23:21:29 +0000

    Ajout de la base de donnée

---
+++ database/migrations/2025_07_16_223706_create_document_justificatifs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('document_justificatifs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('eleve_id')->constrained('eleves')->onDelete('cascade');
+            $table->string("nom_fichier");
+            $table->string("chemin_stockage");
+            $table->string("type_document");
+            $table->string("numero_matricule");
+            $table->dateTime('date_upload')->useCurrent();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('document_justificatifs');
+    }
+};
